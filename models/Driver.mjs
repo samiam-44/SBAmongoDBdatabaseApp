3e8430cf3194b5db3fed12fcf1b86e8d7a66707f
samiam-44 <uriarte.samantha101@gmail.com>
2025-04-28 12:46:24 -0600

    added comments

--- models/Driver.mjs
+++ models/Driver.mjs
@@ -1,20 +1,20 @@
 import mongoose from 'mongoose';
 
-const driverSchema = new mongoose.Schema ({
-    name: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-    },
-    vehicle: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Vehicle',
-        default: null,
+const driverSchema = new mongoose.Schema ({ //Defines Schemma for the model of driver
+    name: { //Drivers name
+        type: String, //In a string type
+        required: true, //Driver name Reuired
+        unique: true, // No 2 drivers can have the same name
+        trim: true, // This ignores the spaces before or after the name "trims" white space
+    }, 
+    vehicle: { //Vehicle feild to link the driver with a vehivle
+        type: mongoose.Schema.Types.ObjectId, //Stores the object if of a vehicle
+        ref: 'Vehicle', //Refercne to vehivle model >>> creates a relationship to driver
+        default: null, //Default value if no vehicle is assigned to the driver
     },
 },
- { timestamps: true });
+ { timestamps: true }); //Logs the time it each item was created at and updated at
 
- const Driver = mongoose.model('Driver, driverSchema');
+ const Driver = mongoose.model('Driver, driverSchema'); //Create a model based on schema above
 
  export default Driver
