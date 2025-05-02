fda5e63c092bff83ffacca0152f1784970e50a32
samiam-44 <uriarte.samantha101@gmail.com>
2025-05-01 20:36:25 -0600

    Faction schema created

---
+++ models/Faction.mjs
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+//Strcture for faction
+const factionSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true, // Faction must have name
+    unique: true, // cant be more than one faction name
+    trim: true,
+    index: true // Indexed for faster lookup
+  },
+  territory: {
+    type: String, // Describes their controlled land like the wastelands or  like the Citadel
+    required: true,
+    trim: true
+  },
+  motto: { //Like the valhalla motto from the citdel
+    type: String, 
+    default: 'No motto provided.'
+  },
+  alliedFactions: [{ 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'Faction' // Self-referencing array for allies
+  }]
+}, { timestamps: true }); // Adds createdAt and updatedAt fields
+
+// Creates and export the model
+const Faction = mongoose.model('Faction', factionSchema);
+
+export default Faction;
